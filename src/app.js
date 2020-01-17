ecd474d10d38e8b32a16c94610dfb350bd5cd025
unknown <dennisjianbin@gmail.com>
2020-01-18 00:57:32 +0100

    adds file which was worked from another computer

--- src/app.js
+++ src/app.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./composition/header";
 
-
-
 class App extends React.Component {
   render() {
-    return <div>
-      <Header/>
-        Hello {this.props.name}, endringer for npm start
-        </div>;
+    return (
+      <div>
+        <Header />
+        {this.props.name}
+      </div>
+    );
   }
 }
 
 var mount = document.getElementById("app");
-ReactDOM.render(<App name="Dennis" />, mount);
+ReactDOM.render(<App name="This is the droptracker" />, mount);
--- src/composition/header.jsx
+++ src/composition/header.jsx
@@ -1,18 +1,21 @@
-import React, {Component} from 'react';
-import './header.less';
+import React, { Component } from "react";
+import "./header.less";
 
 class Header extends Component {
-
   render() {
     return (
-        <div className="header">
-          <h1>Dette er Headeren</h1>
-          <p>reqs: x-axis: players</p>
-          <p>reqs: y-axis: lootentries</p>
-          <p>date</p>
-          <p>sum of drops</p>
-          <p>splitsum</p>
-        </div>
+      <div className="header">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </div>
     );
   }
 }
